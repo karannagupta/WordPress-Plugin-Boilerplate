e36c0829223184ef59989aadfee5e36d51fc6d76
Karan Gupta <nuancedesignstudio@users.noreply.github.com>
2017-09-08 17:45:48 +0530

    Renaming plugin-name to wp-plugin-name This makes it easier to perform a find and replace as plugin-name is also used in variables

---
+++ wp-plugin-name/inc/admin/views/html-wp-plugin-name-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    WP_Plugin_Name
+ * @subpackage WP_Plugin_Name/inc/admin/views
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
